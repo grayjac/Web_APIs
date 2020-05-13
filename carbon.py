1eef1aa4e7b1589b9b85ca887c845542631603af
grayjac <grayjac@oregonstate.edu>
2020-05-13 16:39:35 -0700

    This is the last commit

--- carbon.py
+++ carbon.py
@@ -9,9 +9,10 @@
 import time
 from datetime import datetime as dt
 import requests
-import urllib.request as req  # Request library that lets you get data from web
 import json  # JSON module
 import os.path
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 def get_current_day(unix=time.time()):
@@ -30,7 +31,7 @@ def query_carbon(date=get_current_day(), use_cache=True):
     :return: Forecast data?
     """
 
-    if use_cache == True:
+    if use_cache:
         if os.path.exists('data/carbon_{}.json'.format(date)):
             with open('data/carbon_{}.json'.format(date), 'r') as readfile:
                 return json.load(readfile)
@@ -54,7 +55,31 @@ def query_carbon(date=get_current_day(), use_cache=True):
     return data_dict
 
 
+def plot_carbon(date=get_current_day()):
+    """
+    Outputs a plot of the predicted and realized carbon intensities for a given date.
+    :param date: Date of interest for forecast data. ISO 8601 formatted 'YYYY-MM-DD'.
+    :return: Plot.
+    """
+
+    data_dict = query_carbon(date)
+    t = np.linspace(0, 24, num=len(data_dict['data']), endpoint=False)
+    actual = []
+    forecast = []
+
+    for i in range(len(data_dict['data'])):
+        actual.append(data_dict['data'][i]['intensity']['actual'])
+        forecast.append(data_dict['data'][i]['intensity']['forecast'])
+
+    actual_plt = plt.plot(t, actual, label="Actual Intensity")
+    forecast_plt = plt.plot(t, forecast, label='Forecast Intensity')
+    plt.legend(loc='upper left')
+    plt.title('{} Carbon Intensity vs Time'.format(date))
+    plt.xlabel('Time (hrs from midnight)')
+    plt.ylabel('Carbon Intensity')
+
+    return plt.savefig('plots/carbon_{}.png'.format(date))
+
+
 if __name__ == '__main__':
-    print(type(query_carbon('2020-05-12')))
-
-
+    plot_carbon('2020-04-15')
